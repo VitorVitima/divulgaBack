a883a3c03be18fe524ce45c070b35dc2347d61d3
VitorVitima <yukkiegg@gmail.com>
2023-03-18 06:07:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ mongoose.connect(url).then(()=>console.log('Mongoose conectou'))
 
 const storage = multer.diskStorage({
     destination: (req, file, callback)=>{
-        callback(null, path.resolve('/var/task/var/public/imgs/'))
+        callback(null, path.resolve('/tmp/var/public/imgs/'))
     },
     filename: async (req, file, callback)=>{
         let nameImg2 = `${data}_${file.originalname}`
