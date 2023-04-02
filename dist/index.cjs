1fc15ce14c117b6452dfd0e967913552c1f508ea
VitorVitima <yukkiegg@gmail.com>
2023-04-02 15:38:45 -0300

    Update index.cjs

--- dist/index.cjs
+++ dist/index.cjs
@@ -52,46 +52,22 @@ var userSchema = new import_mongoose.default.Schema({
 var user_default = import_mongoose.default.model("User", userSchema);
 
 // src/index.js
-var import_multer = __toESM(require("multer"), 1);
 var import_cors = __toESM(require("cors"), 1);
-var import_path = __toESM(require("path"), 1);
 var import_mongoose2 = __toESM(require("mongoose"), 1);
-var import_url = require("url");
-var import_meta = {};
-var __filename = (0, import_url.fileURLToPath)(import_meta.url);
-var __dirname = import_path.default.dirname(__filename);
 var app = (0, import_express.default)();
 var port = process.env.PORT || 3001;
-var data = (/* @__PURE__ */ new Date()).getTime();
 var url = "mongodb+srv://dados:ZeOWIzee7yBJOEp9@cluster0.f1egadi.mongodb.net/?retryWrites=true&w=majority";
 import_mongoose2.default.connect(url).then(() => console.log("Mongoose conectou")).catch(() => console.log("Mongoose n\xE3o conectou"));
-var storage = import_multer.default.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, import_path.default.resolve("./imgs/"));
-  },
-  filename: (req, file, callback) => {
-    let nameImg2 = `${data}_${file.originalname}`;
-    callback(null, nameImg2);
-  }
-});
-var upload = (0, import_multer.default)({ storage });
 app.use((0, import_cors.default)());
-app.use("/imgEs", import_express.default.static(import_path.default.resolve(__dirname, "imgs")));
 app.use(import_express.default.json());
-app.post("/uploadImg", upload.single("image"), (req, res) => {
-  const mensagem = "Foto arquivada";
-  return res.json(mensagem);
-});
 app.post("/register", async (req, res) => {
-  const { nome } = await req.body;
+  const { nome } = req.body;
   const { cep } = req.body;
   const { telefone } = req.body;
   const { endereco } = req.body;
   const { estado } = req.body;
   const { categoria } = req.body;
-  const { imgName } = await req.body;
-  console.log(imgName);
-  const img2 = `${data}_${imgName}`;
+  const { img } = req.body;
   const obj = {
     nome,
     cep,
@@ -99,7 +75,7 @@ app.post("/register", async (req, res) => {
     endereco,
     estado,
     categoria,
-    img: img2
+    img
   };
   const newUser = user_default.create(obj);
   return res.json(newUser);
